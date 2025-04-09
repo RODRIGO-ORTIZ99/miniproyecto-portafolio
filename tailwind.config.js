f48c9c9839e4a735e7b95448f3c3e2bca448765d
Rodri <hello@192.168.1.8>
2025-04-08 23:30:36 -0600

    segundo commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    purge: ['./*.html'], // Estilos utilizados en HTML
+    darkMode: false,
+    theme: {
+        extend: {
+            colors: {
+                dark: '#0D1117',
+                light: '#FFFFFF',
+                highlight: '#E3FF6A',
+                neonGreen: '#A4FF7A',
+                deepBlue: '#1E293B',
+                softGray: '#8892B0',
+            },
+            backgroundImage: {
+                'gradient-main': 'linear-gradient(135deg, #1E293B 0%, #0D1117 100%)',
+            },
+        },
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+}
